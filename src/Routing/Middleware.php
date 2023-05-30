9fbc8cce01b9b4ed401bc30a097f2f16635e6454
haejin <haejin.yang91@gmail.com>
2023-05-30 19:49:07 +0900

    refactor: Middleware는 내부 구현 없이 껍데기만 제공한다. 그래서 interface로 변경

--- src/Routing/Middleware.php
+++ src/Routing/Middleware.php
@@ -7,7 +7,7 @@ namespace Thumbsupcat\IcedAmericano\Routing;
  * 미들웨어는 라우터에 등록되어 컨트롤러 이전에 호출된다.
  * 로그인이 되었는지 거르거나, 미리 정의된 데이터가 빠지진 않았는지(토큰, 헤더 등)를 확인하고 거른다.
  */
-abstract class Middleware
+interface Middleware
 {
-    abstract public static function process();
+    public static function process();
 }
